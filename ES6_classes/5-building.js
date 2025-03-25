94a4ff0175dfa71f64f2268669cbcc87444dbad6
Mollydayne <cl.w.perez@gmail.com>
2025-03-25 10:53:29 +0100

    T5 again

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,11 +1,16 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-
-    if (new.target !== Building &&
-        typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
     }
+
+    if (new.target !== undefined && new.target !== Building) {
+      if (typeof new.target.prototype.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+
+    this._sqft = sqft;
   }
 
   get sqft() {
