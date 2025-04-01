a8ef5037251988b9d637e7937f4d9a9bf9cf4117
Mollydayne <cl.w.perez@gmail.com>
2025-04-01 10:45:50 +0200

    Putting export at the end

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,7 @@
-export default function getResponseFromAPI() {
+function getResponseFromAPI() {
   return new Promise((resolve) => {
     resolve(true);
   });
 }
+
+export default getResponseFromAPI;
