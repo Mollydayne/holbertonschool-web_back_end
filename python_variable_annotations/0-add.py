5bf0fdf24b803535381edc1ef6deaa1f89f7c117
Mollydayne <cl.w.perez@gmail.com>
2025-04-23 10:03:30 +0200

    Adding spaces for pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+
 """
 Task :  0- Basic annotations - add
 """
+
 
 def add(a: float, b: float) -> float:
     """
     Function that adds two float numbers
     """
+
     return a + b
