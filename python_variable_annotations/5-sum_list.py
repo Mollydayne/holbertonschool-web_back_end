40f9ba329586e7dc1b83c2b3bf1cd7ca7ecf1ccc
Mollydayne <cl.w.perez@gmail.com>
2025-04-23 10:53:37 +0200

    Task 5 : " Complex types", need to import List from typing to handle it

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+from typing import List
 
 """
 Task 5. Complex types - list of floats
 """
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Function that returns the sum of a list as floats
     """
