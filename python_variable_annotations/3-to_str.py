7cdffbc7b6c875ac9b2b5b6ebd7fd82c372195ec
Mollydayne <cl.w.perez@gmail.com>
2025-04-23 10:25:46 +0200

    Task 3. Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Task 3. Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function that returns the string representation of a float
+    """
+    return str(n)
