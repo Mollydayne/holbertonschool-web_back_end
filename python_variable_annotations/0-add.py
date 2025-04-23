7590c4b09eb9d506cf0721b0a22a339b06edce04
Mollydayne <cl.w.perez@gmail.com>
2025-04-23 10:01:47 +0200

    Adding documentation for task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """
-Task :  0-add
+Task :  0- Basic annotations - add
 """
 
 def add(a: float, b: float) -> float:
+    """
+    Function that adds two float numbers
+    """
     return a + b
