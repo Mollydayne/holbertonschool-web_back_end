38baaa1be6366e06a39bb44e2bcc2b24107ec438
Mollydayne <cl.w.perez@gmail.com>
2025-04-24 12:39:29 +0200

    adding a line at EOF for task 1

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+Asyncio : not a harry potter spell,
+module to let Python wait while somthing else is taking time to do
+List : module to know what type of data we deal with
+wait_random : Function previously implemented in file 0 to create a wait
+routine with a random delay
+"""
+
+import asyncio
+from typing import List
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+"""
+Task 2. Measure the runtime
+"""
