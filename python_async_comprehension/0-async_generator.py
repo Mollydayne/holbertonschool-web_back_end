33f39019e3b1da68a84abc08b0b51214afdc798d
Mollydayne <cl.w.perez@gmail.com>
2025-04-28 10:25:01 +0200

    Adding type to Task 0 too

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,18 +4,19 @@
 Asyncio : not a harry potter spell,
 module to let Python wait while somthing else is taking time to do
 Random : module to have a random float for delay
+List : module to know what type of data we deal with
 """
 
 import asyncio
 import random
-
+from typing import List
 
 """
 Task 0. Async Generator
 """
 
 
-async def async_generator():
+async def async_generator() -> List[float]:
     """
     Function that will loop 10 times and generate a random
     number with 1 second delay
