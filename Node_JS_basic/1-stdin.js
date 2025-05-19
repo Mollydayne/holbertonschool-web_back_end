e6bfc7ebdffe86b5fc700e76a1a4827bc26b3902
Mollydayne <cl.w.perez@gmail.com>
2025-05-19 12:05:56 +0200

    Task until 2

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+// Affiche le message d’accueil
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+// Lis l'entrée utilisateur
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim(); // Nettoie l'entrée
+  process.stdout.write(`Your name is: ${name}\n`);
+});
+
+// Quand l’entrée standard est terminée
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
