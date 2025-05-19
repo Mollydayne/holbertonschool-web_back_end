6ea8b398ca13907e3d095f97dcbe71cc83ac99c3
Mollydayne <cl.w.perez@gmail.com>
2025-05-19 11:43:45 +0200

    Adding commentary to task 0

--- Node_JS_basic/0-main.js
+++ Node_JS_basic/0-main.js
@@ -1,3 +1,8 @@
-const displayMessage = require('./0-console');
+// Define a function named displayMessage that takes a string as argument
+function displayMessage(message) {
+  // Print the string to the standard output (console)
+  console.log(message);
+}
 
-displayMessage("Hello NodeJS!");
+// Export the function so it can be imported in another file
+module.exports = displayMessage;
