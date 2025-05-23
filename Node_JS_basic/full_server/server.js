c5bcc4873631205ca2045e861b1191ce84cad8f6
Mollydayne <cl.w.perez@gmail.com>
2025-05-23 13:40:40 +0200

    Still tryin on 8. Organize a complex HTTP server using Express

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,11 +1,8 @@
 import express from 'express';
-import router from './routes';
+import router from './routes/index';
 
 const app = express();
-const port = 1245;
-
 app.use('/', router);
 
-app.listen(port);
-
+app.listen(1245);
 export default app;
